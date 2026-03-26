1530e0c4fffb6fea4b6f326f192d2d7e820e846f
SkyMazter <comunidadoscar@gmail.com>
2026-03-26 15:59:54 -0400

    added new func for general scripts

--- CLI_Tools/pnk-config/pnk-conf.cpp
+++ CLI_Tools/pnk-config/pnk-conf.cpp
@@ -10,6 +10,20 @@
 
 using namespace std;
 
+void runScript(string file, string arg) {
+  pid_t pid = fork();
+
+  const char *c_file = file.c_str();
+  const char *c_arg = arg.c_str();
+  char *args[] = {(char *)c_file, (char *)c_arg};
+
+  if (pid == 0) {
+    execlp(args[0], args[1], NULL);
+    perror("unable to execure command");
+  } else {
+  }
+}
+
 bool isDockerInstalled() {
   pid_t pid = fork();
   if (pid == 0) {
@@ -26,58 +40,57 @@ bool isDockerInstalled() {
   return false;
 }
 
-void getDockerStat() {
-  int pipefd[2];
-  pipe(pipefd); // pipefd[0] = read, pipefd[1] = write
-  pid_t pid = fork();
+// void getDockerStat() {
+//   int pipefd[2];
+//   pipe(pipefd); // pipefd[0] = read, pipefd[1] = write
+//   pid_t pid = fork();
 
-  if (pid == 0) {
-    close(pipefd[0]); // close unused read end
+//   if (pid == 0) {
+//     close(pipefd[0]); // close unused read end
 
-    // Redirect stdout to pipe
-    dup2(pipefd[1], STDOUT_FILENO);
-    close(pipefd[1]);
+//     // Redirect stdout to pipe
+//     dup2(pipefd[1], STDOUT_FILENO);
+//     close(pipefd[1]);
 
-    execlp("docker", "docker", "ps", NULL);
+//     execlp("docker", "docker", "ps", NULL);
 
-    perror("unable to execure command");
-    _exit(1);
-  } else if (pid > 0) {
-    cout << "This is the parent process\n";
+//     perror("unable to execure command");
+//     _exit(1);
+//   } else if (pid > 0) {
+//     cout << "This is the parent process\n";
 
-    close(pipefd[1]); // close unused write end
+//     close(pipefd[1]); // close unused write end
 
-    string output;
-    char buffer[1024];
-    ssize_t count;
+//     string output;
+//     char buffer[1024];
+//     ssize_t count;
 
-    // Read child output
-    while ((count = read(pipefd[0], buffer, sizeof(buffer))) > 0) {
-      output.append(buffer, count);
-    }
+//     // Read child output
+//     while ((count = read(pipefd[0], buffer, sizeof(buffer))) > 0) {
+//       output.append(buffer, count);
+//     }
 
-    cout << "Captured:\n" << output;
+//     cout << "Captured:\n" << output;
 
-    close(pipefd[0]);
-    waitpid(pid, NULL, 0); // wait for child
-  } else {
-    cerr << "Fork failed\n";
-  }
-}
+//     close(pipefd[0]);
+//     waitpid(pid, NULL, 0); // wait for child
+//   } else {
+//     cerr << "Fork failed\n";
+//   }
+// }
 
 int main() {
-  // cout << isDockerInstalled() << endl;
-
   if (isDockerInstalled() == true) {
     cout << "Docker is installed! Proceeding to next check..." << endl;
   } else {
-    cout << "Docker is not installed!, Would you like to install docker?"
-         << endl;
+    cout
+        << "Docker is not installed!, Would you like to install docker?: (y/N)";
 
     char ans;
     cin >> ans;
     if (ans == 'y') {
 
+      runScript("./docker_install", "./docker_install");
     } else {
       cout << "Closing Script..." << endl;
     }
