fb74f5d8f4f2f7af4570b8d9aa48c89f5b94940a
SkyMazter <comunidadoscar@gmail.com>
2026-03-27 17:40:53 -0400

    updated if else function for container checker

--- CLI_Tools/pnk-config/pnk-conf.cpp
+++ CLI_Tools/pnk-config/pnk-conf.cpp
@@ -63,45 +63,6 @@ bool isDockerInstalled() {
   return false;
 }
 
-// void getDockerStat() {
-//   int pipefd[2];
-//   pipe(pipefd); // pipefd[0] = read, pipefd[1] = write
-//   pid_t pid = fork();
-
-//   if (pid == 0) {
-//     close(pipefd[0]); // close unused read end
-
-//     // Redirect stdout to pipe
-//     dup2(pipefd[1], STDOUT_FILENO);
-//     close(pipefd[1]);
-
-//     execlp("docker", "docker", "ps", NULL);
-
-//     perror("unable to execure command");
-//     _exit(1);
-//   } else if (pid > 0) {
-//     cout << "This is the parent process\n";
-
-//     close(pipefd[1]); // close unused write end
-
-//     string output;
-//     char buffer[1024];
-//     ssize_t count;
-
-//     // Read child output
-//     while ((count = read(pipefd[0], buffer, sizeof(buffer))) > 0) {
-//       output.append(buffer, count);
-//     }
-
-//     cout << "Captured:\n" << output;
-
-//     close(pipefd[0]);
-//     waitpid(pid, NULL, 0); // wait for child
-//   } else {
-//     cerr << "Fork failed\n";
-//   }
-// }
-//
 bool isContainerRunning(string container_name) {
   int pipefd[2];
   pipe(pipefd); // pipefd[0] = read, pipefd[1] = write
@@ -119,8 +80,10 @@ bool isContainerRunning(string container_name) {
     close(pipefd[1]);
 
     execlp("docker", "container", "ps", "--filter", c_container_name, NULL);
+
     perror("unable to run docker command");
-    _exit(0);
+    _exit(127);
+
   } else {
     close(pipefd[1]);
 
@@ -144,8 +107,9 @@ int main() {
 
   if (isDockerInstalled() == true) {
     cout << "Docker is installed! Proceeding to next check..." << endl;
-    cout << isContainerRunning("suspicious_euler");
-    // cout << runScript({"docker", "ps", "--filter", "name=suspicious_euler"});
+    isContainerRunning("suspicious_euler")
+        ? cout << "container is running" << endl
+        : cout << "container is not running" << endl;
   } else {
     cout << "Docker is not installed!, Would you like to install docker?"
             "(y/N): ";
