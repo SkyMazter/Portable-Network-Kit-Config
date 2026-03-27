7454a02b1aa6f8ae274608f344481737d18ab81b
SkyMazter <comunidadoscar@gmail.com>
2026-03-27 14:49:00 -0400

    updated script

--- CLI_Tools/pnk-config/pnk-conf.cpp
+++ CLI_Tools/pnk-config/pnk-conf.cpp
@@ -1,23 +1,21 @@
 #include <cstddef>
 #include <cstdio>
 #include <iostream>
+#include <regex>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <vector>
 
 using namespace std;
 
-string runScript(string file, string arg) {
+string runScript(const vector<string> &args) {
   int pipefd[2];
   pipe(pipefd); // pipefd[0] = read, pipefd[1] = write
   pid_t pid = fork();
-
-  const char *c_file = file.c_str();
-  const char *c_arg = arg.c_str();
-  char *args[] = {(char *)c_file, (char *)c_arg};
 
   if (pid == 0) {
     close(pipefd[0]);
@@ -25,8 +23,14 @@ string runScript(string file, string arg) {
     dup2(pipefd[1], STDOUT_FILENO);
     close(pipefd[1]);
 
-    execlp(args[0], args[1], NULL);
-    perror("unable to execure command");
+    std::vector<char *> cargs;
+    for (const auto &arg : args) {
+      cargs.push_back(const_cast<char *>(arg.c_str()));
+    }
+    cargs.push_back(NULL);
+    execvp(cargs[0], cargs.data());
+    perror("unable to execute command");
+    _exit(127);
   } else {
     close(pipefd[1]);
     string output;
@@ -99,15 +103,36 @@ bool isDockerInstalled() {
 // }
 //
 bool isContainerRunning(string container_name) {
+  int pipefd[2];
+  pipe(pipefd); // pipefd[0] = read, pipefd[1] = write
+
   pid_t pid = fork();
-  container_name = "name=" + container_name;
-  const char *c_container_name = container_name.c_str();
+
+  string arg_container_name = "name=" + container_name;
+  const char *c_container_name = arg_container_name.c_str();
   const char *args[] = {(char *)c_container_name};
 
   if (pid == 0) {
+    close(pipefd[0]);
+
+    dup2(pipefd[1], STDOUT_FILENO);
+    close(pipefd[1]);
+
     execlp("docker", "container", "ps", "--filter", c_container_name, NULL);
     perror("unable to run docker command");
+    _exit(0);
   } else {
+    close(pipefd[1]);
+
+    string output;
+    ssize_t count;
+    char buffer[1024];
+
+    while ((count = read(pipefd[0], buffer, sizeof(buffer))) > 0) {
+      output.append(buffer, count);
+    }
+
+    regex pattern("\\b" + container_name + "\\b");
   }
 
   return false;
@@ -117,16 +142,15 @@ int main() {
 
   if (isDockerInstalled() == true) {
     cout << "Docker is installed! Proceeding to next check..." << endl;
-    isContainerRunning("mariadb");
+    // isContainerRunning("mariadb");
+    cout << runScript({"docker", "ps", "--filter", "name=suspicious_euler"});
   } else {
     cout << "Docker is not installed!, Would you like to install docker?"
             "(y/N): ";
     char ans;
     cin >> ans;
     if (ans == 'y') {
-      cout << runScript("./dummy.sh", "./dummy.sh"
-                                      "dummy.sh")
-           << endl;
+      cout << runScript({"bash", "./dummy.sh"}) << endl;
     } else {
       cout << "Closing Script..." << endl;
     }
