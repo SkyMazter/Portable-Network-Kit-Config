6846cffd0ad1044cd2d876677e7d5bb363064d86
SkyMazter <comunidadoscar@gmail.com>
2026-03-30 13:14:47 -0400

    added new conditionals

--- CLI_Tools/pnk-config/pnk-conf.cpp
+++ CLI_Tools/pnk-config/pnk-conf.cpp
@@ -96,20 +96,46 @@ bool isContainerRunning(string container_name) {
     }
 
     regex pattern("\\b" + container_name + "\\b");
-    bool isActive = regex_search(output, pattern);
-    return isActive;
+    bool is_active = regex_search(output, pattern);
+    is_active ? cout << "Container " << container_name
+                     << " is running normally..." << endl
+              : cout << container_name << " is not running..." << endl;
+
+    return is_active;
   }
 
   return false;
+}
+
+void checkContainerStatus(const string container_name) {
+  const string script_name = "./" + container_name + "_install.sh";
+
+  if (isContainerRunning(container_name))
+    return;
+
+  regex pattern("\\b" + container_name + "\\b");
+  const string shell_output = runScript({"docker", "image", "list"});
+  bool is_installed = regex_search(shell_output, pattern);
+
+  if (is_installed) {
+    cout << "" << endl;
+  } else {
+
+    cout << container_name
+         << " is not installed yet!, Would you like to run it's install script?"
+            "(y/N): ";
+    char ans;
+    cin >> ans;
+    (ans == 'y') ? cout << runScript({"bash", script_name}) << endl
+                 : cout << "Closing Script..." << endl;
+  }
 }
 
 int main() {
 
   if (isDockerInstalled() == true) {
     cout << "Docker is installed! Proceeding to next check..." << endl;
-    isContainerRunning("suspicious_euler")
-        ? cout << "container is running" << endl
-        : cout << "container is not running" << endl;
+    checkContainerStatus("wordpress");
   } else {
     cout << "Docker is not installed!, Would you like to install docker?"
             "(y/N): ";
