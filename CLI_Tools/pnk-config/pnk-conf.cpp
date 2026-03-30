1f44909985fa8c06ec74f4d7078954451f88be6b
SkyMazter <comunidadoscar@gmail.com>
2026-03-30 14:40:28 -0400

    now loops through container names to determine install status

--- CLI_Tools/pnk-config/pnk-conf.cpp
+++ CLI_Tools/pnk-config/pnk-conf.cpp
@@ -12,32 +12,38 @@
 
 using namespace std;
 
-string runScript(const vector<string> &args) {
+string runScript(const vector<string> &args)
+{
   int pipefd[2];
   pipe(pipefd); // pipefd[0] = read, pipefd[1] = write
   pid_t pid = fork();
 
-  if (pid == 0) {
+  if (pid == 0)
+  {
     close(pipefd[0]);
 
     dup2(pipefd[1], STDOUT_FILENO);
     close(pipefd[1]);
 
     std::vector<char *> cargs;
-    for (const auto &arg : args) {
+    for (const auto &arg : args)
+    {
       cargs.push_back(const_cast<char *>(arg.c_str()));
     }
     cargs.push_back(NULL);
     execvp(cargs[0], cargs.data());
     perror("unable to execute command");
     _exit(127);
-  } else {
+  }
+  else
+  {
     close(pipefd[1]);
     string output;
     char buffer[1024];
     ssize_t count;
 
-    while ((count = read(pipefd[0], buffer, sizeof(buffer))) > 0) {
+    while ((count = read(pipefd[0], buffer, sizeof(buffer))) > 0)
+    {
       output.append(buffer, count);
     }
 
@@ -47,15 +53,20 @@ string runScript(const vector<string> &args) {
   return "There was an Error executing the command...";
 }
 
-bool isDockerInstalled() {
+bool isDockerInstalled()
+{
   pid_t pid = fork();
-  if (pid == 0) {
+  if (pid == 0)
+  {
     execlp("docker", "docker", "--version", NULL);
     _exit(127);
-  } else {
+  }
+  else
+  {
     int status;
     waitpid(pid, &status, 0);
-    if (WIFEXITED(status)) {
+    if (WIFEXITED(status))
+    {
       int exitCode = WEXITSTATUS(status);
       return exitCode == 0; // return true
     }
@@ -63,7 +74,8 @@ bool isDockerInstalled() {
   return false;
 }
 
-bool isContainerRunning(string container_name) {
+bool isContainerRunning(string container_name)
+{
   int pipefd[2];
   pipe(pipefd); // pipefd[0] = read, pipefd[1] = write
 
@@ -73,7 +85,8 @@ bool isContainerRunning(string container_name) {
   const char *c_container_name = arg_container_name.c_str();
   const char *args[] = {(char *)c_container_name};
 
-  if (pid == 0) {
+  if (pid == 0)
+  {
     close(pipefd[0]);
 
     dup2(pipefd[1], STDOUT_FILENO);
@@ -83,15 +96,17 @@ bool isContainerRunning(string container_name) {
 
     perror("unable to run docker command");
     _exit(127);
-
-  } else {
+  }
+  else
+  {
     close(pipefd[1]);
 
     string output;
     ssize_t count;
     char buffer[1024];
 
-    while ((count = read(pipefd[0], buffer, sizeof(buffer))) > 0) {
+    while ((count = read(pipefd[0], buffer, sizeof(buffer))) > 0)
+    {
       output.append(buffer, count);
     }
 
@@ -103,23 +118,50 @@ bool isContainerRunning(string container_name) {
   return false;
 }
 
-int main() {
+int main()
+{
 
-  if (isDockerInstalled() == true) {
-    cout << "Docker is installed! Proceeding to next check..." << endl;
-    isContainerRunning("suspicious_euler")
-        ? cout << "container is running" << endl
-        : cout << "container is not running" << endl;
-  } else {
+  if (!isDockerInstalled())
+  {
     cout << "Docker is not installed!, Would you like to install docker?"
             "(y/N): ";
     char ans;
     cin >> ans;
-    if (ans == 'y') {
+    if (ans == 'y')
+    {
       cout << runScript({"bash", "./dummy.sh"}) << endl;
-    } else {
+    }
+    else
+    {
       cout << "Closing Script..." << endl;
+      return 0;
     }
   }
+
+  cout << "Docker is installed! Proceeding to next check..." << endl;
+
+  const vector<string> container_names = {"dummy", "dummy", "dummy"};
+
+  for_each(container_names.begin(), container_names.end(), [](string container_name)
+           {
+             bool container_status = isContainerRunning(container_name);
+             if (!container_status)
+             {
+               cout << "The " + container_name + " container is not running...\n"
+                    << "Would you like to run the installation script? (y/N): ";
+               char ans;
+               cin >> ans;
+               if (ans == 'y')
+               {
+                 cout << runScript({"bash", "./" + container_name + "_installation.sh"}) << endl;
+               }
+               else
+               {
+                 cout << "Skipping Installation Script..." << endl;
+                 exit;
+               }
+             }
+           });
+
   return 0;
 }
