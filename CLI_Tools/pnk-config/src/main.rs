6e910fbc81cddd447e6a7b9e8e32cbe5dd6c447a
SkyMazter <comunidadoscar@gmail.com>
2026-04-22 19:46:54 -0400

    fixed race condition

--- CLI_Tools/pnk-config/src/main.rs
+++ CLI_Tools/pnk-config/src/main.rs
@@ -1,3 +1,35 @@
+use clap::Parser;
+use std::process::{Command, Stdio};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    service: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli: Cli = Cli::parse();
+
+    match Command::new("figlet")
+        .args(["PNK Configuration", "-f", "mini"])
+        .stderr(Stdio::piped())
+        .stdout(Stdio::inherit())
+        .output()
+    {
+        Ok(_) => println!("_|_|_"),
+        Err(e) => eprintln!("Error: {}", e),
+    }
+
+    if cli.service != None {
+        println!(">> Searching for {} container...", cli.service.unwrap())
+    }
+
+    //Run Figlet
+    //Check for internet connection
+    //Check for bash scripts
+    // check if docker is installed
+    //  install docker if not installed
+    // check if docker containers are running
+    //  install images if not installed, start containers if not running
 }
